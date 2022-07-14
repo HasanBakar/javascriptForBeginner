55249d2d699b3c93cb665ddcd61a8187ca2838ba
HasanBakar <abubakar.cse@gmail.com>
2022-07-14 15:30:11 +0600

    how to fine data type of variable with typeof keyword

--- dataType.js
+++ dataType.js
@@ -1,4 +1,4 @@
 var strType = "I want to be a happy programmer in the world!";
-var idNumber = 345;
-console.log(strType,idNumber);
-console.log(typeof strType, typeof idNumber);
+var numType = 345;
+console.log(strType = typeof strType);
+console.log(numType = typeof numType);
