4024abc1d8a616d6cbb65185079ec1926467e8c2
HasanBakar <abubakar.cse@gmail.com>
2022-07-15 15:28:31 +0600

    some methods such as indexOf(), split(), toUpperCase(),toLowerCase(),

--- someMethods.js
+++ someMethods.js
@@ -4,3 +4,11 @@ console.log(rochona.toUpperCase());
 var paragraph = "IT IS VERY INTERESING FOR LEARNING JAVACRIPT PROGRAMMING LANGUAGE. SO I WANT TO LEARN WITH HABLU PROGRAMMER TEAM. TAKE A RISK AND GAIN YOUR FUTURE."
 
 console.log(paragraph.toLowerCase());
+
+var friendsList = "hasan rakib sakib kamal rafi sayde";
+
+console.log(friendsList.split("rakib"));
+
+console.log(friendsList.indexOf("sakib"));
+
+
