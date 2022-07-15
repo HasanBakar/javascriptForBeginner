78e0067f75dd04f248baa255a13ff35cf52159ae
HasanBakar <abubakar.cse@gmail.com>
2022-07-15 15:08:02 +0600

    To learn about two methods in javacript toUpperCase ,toLowerCase

---
+++ someMethods.js
@@ -0,0 +1,6 @@
+var rochona = "I want to a great person in the world. as like that mufit mank and denish richis";
+
+console.log(rochona.toUpperCase());
+var paragraph = "IT IS VERY INTERESING FOR LEARNING JAVACRIPT PROGRAMMING LANGUAGE. SO I WANT TO LEARN WITH HABLU PROGRAMMER TEAM. TAKE A RISK AND GAIN YOUR FUTURE."
+
+console.log(paragraph.toLowerCase());
