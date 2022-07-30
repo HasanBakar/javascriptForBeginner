6e3f4781396ed64072528f060ce184f1351924bf
HasanBakar <abubakar.cse@gmail.com>
2022-07-31 03:46:23 +0600

    Basic operation in javascript

---
+++ operation.js
@@ -0,0 +1,14 @@
+var num1 = 39;
+var num2 = 456;
+
+console.log("add =", num1 + num2);
+
+console.log("sub =", num2 - num1);
+
+var total = num1 * num2;
+
+console.log("multi =", total);
+
+console.log("division =", num2 / num1);
+
+console.log("reminder =", num2 % num1);
