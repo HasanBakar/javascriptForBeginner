afbd0d7d54e7e642d529fa42e53c6db861b6f8f3
HasanBakar <abubakar.cse@gmail.com>
2022-07-31 04:03:09 +0600

    Math.something fuction

---
+++ math___fuction.js
@@ -0,0 +1,18 @@
+var num = -4;
+console.log(num);
+var sum = Math.abs(num);
+
+console.log(sum);
+
+var fraction = 344.00002;
+
+sum = fraction;
+sum = Math.round(fraction);
+console.log(sum);
+
+sum = Math.ceil(fraction);
+console.log(sum);
+
+sum = Math.floor(fraction);
+
+console.log(sum);
