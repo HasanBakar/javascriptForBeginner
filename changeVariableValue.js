838941fc268bfc2d8c2bb648149c580e85def996
HasanBakar <abubakar.cse@gmail.com>
2022-08-02 04:17:53 +0600

    variable declaration and variable value changing

---
+++ changeVariableValue.js
@@ -0,0 +1,17 @@
+var x, y, z;
+
+x = 5;
+
+y = 9;
+
+z = 30;
+
+console.log("x =", x, "y =", y, "z =", z);
+
+// changing the variable value 
+
+console.log("the variable value after change");
+
+x = 3; y = 45; z = 598;
+
+console.log("x =", x, "y =", y, "z =", z);
